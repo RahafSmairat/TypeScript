d849202e230a259ef377d947b9f67bae6cfc7c0b
unknown <rahaf.alsmairat@gmail.com>
2025-06-15 03:26:29 +0300

    Final Submission

--- Self Practice/ToDo.js
+++ Self Practice/ToDo.js
@@ -9,71 +9,99 @@ var __assign = (this && this.__assign) || function () {
     };
     return __assign.apply(this, arguments);
 };
+//To initialize the tasks list from the local storage
 var tasksList = JSON.parse(localStorage.getItem("Tasks") || "[]");
+//Function to add a task
 function addTask(event) {
+    //Prevent page load
     event.preventDefault();
+    //Get data from the form
     var taskName = document.getElementById("name").value;
     var taskDescription = document.getElementById("description").value;
+    var taskStartDate = document.getElementById("startDate").value;
+    var taskEndDate = document.getElementById("endDate").value;
     var taskID = tasksList.length + 1;
-    if (taskName.trim() == "" || taskDescription.trim() == "") {
+    //Warning message if inputs are empty
+    if (taskName.trim() == "" || taskDescription.trim() == "" || taskStartDate.trim() == "" || taskEndDate.trim() == "") {
         document.getElementById("message").innerHTML =
             "<div class=\"alert alert-danger\" role=\"alert\">\n                 Please Fill All Feilds!\n             </div>";
         return;
     }
+    //Assign the data from the form to an object
     var task = {
         id: taskID,
         name: taskName,
         description: taskDescription,
-        status: "To Do"
+        status: "To Do",
+        startDate: taskStartDate,
+        endDate: taskEndDate
     };
+    //Add the task to the array
     tasksList.push(task);
+    //Save the task to local storage
     localStorage.setItem("Tasks", JSON.stringify(tasksList));
+    //Display success message
     document.getElementById("message").innerHTML =
-        "<div class=\"alert alert-success\" role=\"alert\">\n    Task Added successfully!\n</div>";
+        "<div class=\"alert alert-success\" role=\"alert\">\n            Task Added successfully!\n         </div>";
+    //Empty form inputs
     document.getElementById("name").value = '';
     document.getElementById("description").value = '';
+    document.getElementById("startDate").value = '';
+    document.getElementById("endDate").value = '';
+    //Display all tasks
     displayTasks();
 }
+//Function to display all tasks
 function displayTasks() {
+    //Get the container that will have the tasks
     var tasksContainer = document.getElementById("tasksContainer");
     if (tasksContainer) {
+        //Empty the container
         tasksContainer.innerHTML = "";
+        //Print each task in the array
         tasksList.forEach(function (task) {
             tasksContainer.innerHTML +=
-                "\n            <tr>\n                <th scope=\"row\">".concat(task.id, "</th>\n                    <td>").concat(task.name, "</td>\n                    <td>").concat(task.description, "</td>\n                    <td>").concat(task.status, "</td>\n                    <td id=\"btns").concat(task.id, "\" class=\"btns\">\n                    <a onclick=\"deleteTask(").concat(task.id, ")\" class=\"text-danger\" data-mdb-tooltip-init title=\"Delete\">\n                      <i class=\"fas fa-trash-alt\"></i>\n                    </a>\n                    </td>\n            </tr>\n            ");
+                "\n            <tr>\n                <th scope=\"row\">".concat(task.id, "</th>\n                    <td>").concat(task.name, "</td>\n                    <td>").concat(task.description, "</td>\n                    <td>").concat(task.startDate, "</td>\n                    <td>").concat(task.endDate, "</td>\n                    <td>").concat(task.status, "</td>\n                    <td id=\"btns").concat(task.id, "\" class=\"btns\">\n                    <a onclick=\"deleteTask(").concat(task.id, ")\" class=\"text-danger\" data-mdb-tooltip-init title=\"Delete\">\n                      <i class=\"fas fa-trash-alt\"></i>\n                    </a>\n                    </td>\n            </tr>\n            ");
+            //Change status button based on the task status
             if (task.status == "To Do") {
                 document.getElementById("btns".concat(task.id)).innerHTML +=
-                    "\n                <a onclick=\"completed(".concat(task.id, ")\" class=\"text-success\" data-mdb-tooltip-init title=\"Completed\">\n                    <i class=\"far fa-square\"></i>\n                </a>\n                ");
+                    "\n                <a onclick=\"changeStatus(".concat(task.id, ")\" class=\"text-success\" data-mdb-tooltip-init title=\"Completed\">\n                    <i class=\"far fa-square\"></i>\n                </a>\n                ");
             }
             else {
                 document.getElementById("btns".concat(task.id)).innerHTML +=
-                    "\n                <a onclick=\"toDo(".concat(task.id, ")\" class=\"text-success\" data-mdb-tooltip-init title=\"Completed\">\n                    <i class=\"fas fa-check-square\"></i>\n                </a>\n                ");
+                    "\n                <a onclick=\"changeStatus(".concat(task.id, ")\" class=\"text-success\" data-mdb-tooltip-init title=\"Completed\">\n                    <i class=\"fas fa-check-square\"></i>\n                </a>\n                ");
             }
         });
     }
 }
 displayTasks();
+//Function to delete a task
 function deleteTask(id) {
+    //Add the tasks except the target id to be deleted
     tasksList = tasksList.filter(function (task) { return task.id !== id; });
+    //Reset the ID 
     tasksList = tasksList.map(function (task, index) { return (__assign(__assign({}, task), { id: index + 1 })); });
+    //Reasssign the tasks list to the local storage
     localStorage.setItem("Tasks", JSON.stringify(tasksList));
+    //Display all tasks
     displayTasks();
 }
-function completed(id) {
+//Change status function
+function changeStatus(id) {
+    //Search for the target id to change the status
     tasksList.forEach(function (task) {
         if (task.id == id) {
-            task.status = "Completed";
+            //Toggle the status
+            if (task.status == "To Do") {
+                task.status = "Completed";
+            }
+            else {
+                task.status = "To Do";
+            }
         }
     });
+    //Reasssign the tasks list to the local storage
     localStorage.setItem("Tasks", JSON.stringify(tasksList));
-    displayTasks();
-}
-function toDo(id) {
-    tasksList.forEach(function (task) {
-        if (task.id == id) {
-            task.status = "To Do";
-        }
-    });
-    localStorage.setItem("Tasks", JSON.stringify(tasksList));
+    //Display all tasks
     displayTasks();
 }
--- Self Practice/ToDo.ts
+++ Self Practice/ToDo.ts
@@ -1,19 +1,31 @@
+
 interface Task {
     id: number;
     name: string;
     description: string;
     status: string;
+    startDate: string;
+    endDate: string;
 }
 
+//To initialize the tasks list from the local storage
 let tasksList: Task[] = JSON.parse(localStorage.getItem("Tasks") || "[]");
 
+//Function to add a task
 function addTask(event) {
+
+    //Prevent page load
     event.preventDefault();
+
+    //Get data from the form
     let taskName = (document.getElementById("name") as HTMLInputElement).value
     let taskDescription = (document.getElementById("description") as HTMLInputElement).value
+    let taskStartDate = (document.getElementById("startDate") as HTMLInputElement).value
+    let taskEndDate = (document.getElementById("endDate") as HTMLInputElement).value
     let taskID = tasksList.length + 1;
 
-    if (taskName.trim() == "" || taskDescription.trim() == "") {
+    //Warning message if inputs are empty
+    if (taskName.trim() == "" || taskDescription.trim() == "" || taskStartDate.trim() == "" || taskEndDate.trim() == "") {
         (document.getElementById("message") as HTMLInputElement).innerHTML =
             `<div class="alert alert-danger" role="alert">
                  Please Fill All Feilds!
@@ -21,30 +33,49 @@ function addTask(event) {
         return
     }
 
+    //Assign the data from the form to an object
     let task: Task = {
         id: taskID,
         name: taskName,
         description: taskDescription,
-        status: "To Do"
+        status: "To Do",
+        startDate: taskStartDate,
+        endDate: taskEndDate
     }
 
+    //Add the task to the array
     tasksList.push(task)
+
+    //Save the task to local storage
     localStorage.setItem("Tasks", JSON.stringify(tasksList));
 
+    //Display success message
     (document.getElementById("message") as HTMLInputElement).innerHTML =
         `<div class="alert alert-success" role="alert">
-    Task Added successfully!
-</div>`;
+            Task Added successfully!
+         </div>`;
 
+    //Empty form inputs
     (document.getElementById("name") as HTMLInputElement).value = '';
     (document.getElementById("description") as HTMLInputElement).value = '';
+    (document.getElementById("startDate") as HTMLInputElement).value = '';
+    (document.getElementById("endDate") as HTMLInputElement).value = '';
+
+    //Display all tasks
     displayTasks()
 }
 
+//Function to display all tasks
 function displayTasks() {
+
+    //Get the container that will have the tasks
     let tasksContainer = document.getElementById("tasksContainer");
+
     if (tasksContainer) {
+        //Empty the container
         tasksContainer.innerHTML = "";
+
+        //Print each task in the array
         tasksList.forEach(task => {
             tasksContainer.innerHTML +=
                 `
@@ -52,6 +83,8 @@ function displayTasks() {
                 <th scope="row">${task.id}</th>
                     <td>${task.name}</td>
                     <td>${task.description}</td>
+                    <td>${task.startDate}</td>
+                    <td>${task.endDate}</td>
                     <td>${task.status}</td>
                     <td id="btns${task.id}" class="btns">
                     <a onclick="deleteTask(${task.id})" class="text-danger" data-mdb-tooltip-init title="Delete">
@@ -60,10 +93,12 @@ function displayTasks() {
                     </td>
             </tr>
             `
+
+            //Change status button based on the task status
             if (task.status == "To Do") {
                 (document.getElementById(`btns${task.id}`) as HTMLTableCellElement).innerHTML +=
                     `
-                <a onclick="completed(${task.id})" class="text-success" data-mdb-tooltip-init title="Completed">
+                <a onclick="changeStatus(${task.id})" class="text-success" data-mdb-tooltip-init title="Completed">
                     <i class="far fa-square"></i>
                 </a>
                 `
@@ -71,7 +106,7 @@ function displayTasks() {
             else {
                 (document.getElementById(`btns${task.id}`) as HTMLTableCellElement).innerHTML +=
                     `
-                <a onclick="toDo(${task.id})" class="text-success" data-mdb-tooltip-init title="Completed">
+                <a onclick="changeStatus(${task.id})" class="text-success" data-mdb-tooltip-init title="Completed">
                     <i class="fas fa-check-square"></i>
                 </a>
                 `
@@ -82,36 +117,45 @@ function displayTasks() {
 
 displayTasks();
 
+//Function to delete a task
 function deleteTask(id: number) {
+
+    //Add the tasks except the target id to be deleted
     tasksList = tasksList.filter(task => task.id !== id);
 
+    //Reset the ID 
     tasksList = tasksList.map((task, index) => ({
         ...task,
         id: index + 1,
     }));
 
+    //Reasssign the tasks list to the local storage
     localStorage.setItem("Tasks", JSON.stringify(tasksList));
+
+    //Display all tasks
     displayTasks()
 }
 
-function completed(id: number) {
+//Change status function
+function changeStatus(id: number) {
+
+    //Search for the target id to change the status
     tasksList.forEach(task => {
         if (task.id == id) {
-            task.status = "Completed"
+            
+            //Toggle the status
+            if(task.status == "To Do"){
+                task.status = "Completed"
+            }
+            else{
+                task.status = "To Do"
+            }
         }
     })
 
+    //Reasssign the tasks list to the local storage
     localStorage.setItem("Tasks", JSON.stringify(tasksList))
-    displayTasks()
-}
 
-function toDo(id: number) {
-    tasksList.forEach(task => {
-        if (task.id == id) {
-            task.status = "To Do"
-        }
-    })
-
-    localStorage.setItem("Tasks", JSON.stringify(tasksList))
+    //Display all tasks
     displayTasks()
 }
